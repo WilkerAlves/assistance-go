45acfb8297f48ab973c1d17497103ad4bb747957
Wilker Alves Nogueira <wilkeralves97@gmail.com>
2022-07-31 14:43:00 -0300

    create interface category_repository

---
+++ src/domain/repository/category_repository.go
@@ -0,0 +1,10 @@
+package repository
+
+import "github.com/WilkerAlves/assistance-go/src/domain/entity"
+
+type ICategoryRepository interface {
+	Create(category entity.Category) error
+	Update(category entity.Category) error
+	Find(id string) (*entity.Category, error)
+	FindByName(name string) (*entity.Category, error)
+}
