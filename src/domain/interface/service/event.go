645a3f144092a1459457f33f90d1cbdc2487c7de
Wilker Alves <wilkeralves97@gmail.com>
2022-08-01 18:33:45 -0300

    event category

--- src/domain/interface/service/event.go
+++ src/domain/interface/service/event.go
@@ -2,4 +2,18 @@ package service
 
 type IEventService interface {
 	Send(event string, body interface{}) error
+	//Read(event string, body interface{}) entity.Category
 }
+
+//
+//type EventCategory struct {
+//	category entity.Category
+//}
+//
+//func (e *EventCategory) Send() error  {
+//
+//}
+//
+//func (e *EventCategory) Read() entity.Category  {
+//
+//}
